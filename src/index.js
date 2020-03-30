67df1776625074bd9dfbb72a1264dc22daace51a
jesusbrito <udvuvvdu29>
2020-03-30 00:08:03 -0600

    Componentes iniciales

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, Platzi Badges!';
 
+// const container = document.getElementById('app');
+
+// container.appendChild(element);
+
+import React from "react";
+import ReactDOM from "react-dom";
+import BadgeNew from "./pages/BadgeNew";
+import "bootstrap/dist/css/bootstrap.css";
+import "../src/global.css";
+
+const container = document.getElementById("app");
+
+// ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ <BadgeNew/>,
+  container
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
